a32ad44cbee90942289df645af67c2fb57ecd6ea
Administrator <skyestzhang@163.com>
2019-10-28 19:04:59 +0800

    components

---
+++ src/components/common/HeadBar.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="head-bar">
+     <div class="logo">
+         <img v-if="logo" :src="logo"/>
+     </div>
+     <div>
+         <a>发布</a>
+     </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HeadBar',
+  components: {
+
+  },
+  props: {
+    logo: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.head-bar{
+    background: #000000;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+}
+.logo{
+    width: 50px;
+    height: 50px;
+}
+a{
+    text-decoration: none;
+    color: #bbb;
+}
+</style>
---
+++ src/components/common/LayoutThumbnail.vue
@@ -0,0 +1,63 @@
+<template>
+ <div class="layout-thum" @click="setLayout">
+     <div v-if="type==='top-nav' || type==='left-top-nav'" class="layout-preview top-nav"></div>
+     <div v-if="type==='left-nav'" class="layout-preview left-nav"></div>
+     <div v-if="type==='left-top-nav'" class="layout-preview left-top-nav"></div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutThumbnail',
+  components: {
+
+  },
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'none'
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    setLayout () {
+      this.$emit('change', this.type)
+    }
+    //   ####################methods####################
+  }
+}
+</script>
+
+<style scoped>
+.layout-thum{
+    width: 74px;
+    height: 53px;
+    border: 1px solid #d8d8d8;
+    position: relative;
+    padding: 4px;
+    margin-bottom: 10px;
+}
+
+.layout-preview{
+    box-sizing: border-box;
+    background: #e5f3ff;
+}
+.top-nav{
+      height: 10px;
+    width: 100%;
+}
+.left-nav{
+      height: 100%;
+    width: 10px;
+}
+
+.left-top-nav{
+      height: 80%;
+    width: 10px;
+}
+</style>
