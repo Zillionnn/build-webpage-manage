5041c462646773dba1bc78c5189d8e98d9120fc6
Administrator <skyestzhang@163.com>
2019-10-31 18:37:10 +0800

    fix

--- src/components/pages/PageConfig.vue
+++ src/components/pages/PageConfig.vue
@@ -68,7 +68,9 @@
         <div class="component-drag" draggable="true" @dragstart="startDrag('text')">文字</div>
         <div class="component-drag" draggable="true" @dragstart="startDrag('pic')">图片</div>
         <div class="component-drag" draggable="true" @dragstart="startDrag('chart-bar')">柱状图</div>
+        <div class="component-drag" draggable="true" @dragstart="startDrag('chart-line')">折线图</div>
       </div>
+
     </div>
     <!-- ###################################画布 操作界面 ########################################-->
     <div class="canvas-body">
@@ -472,7 +474,7 @@ export default {
       if (conf.y) {
         box.y = conf.y - this.canvas.top
       }
-      if (box.type === 'chart-bar') {
+      if (box.type.indexOf('chart') > -1) {
         box.info.style.width = box.width + 'px'
         box.info.style.height = box.height + 'px'
         console.log('currentBox>>', this.currentBox)
@@ -601,43 +603,50 @@ export default {
               ]
             },
             attrs: {
-              options: {
-                color: ['#3398DB'],
-                tooltip: {
-                  trigger: 'axis',
-                  axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-                  }
-                },
-                grid: {
-                  left: '3%',
-                  right: '4%',
-                  bottom: '3%',
-                  containLabel: true
-                },
-                xAxis: [
-                  {
-                    type: 'category',
-                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                    axisTick: {
-                      alignWithLabel: true
-                    }
-                  }
-                ],
-                yAxis: [
-                  {
-                    type: 'value'
-                  }
-                ],
-                series: [
-                  {
-                    name: '直接访问',
-                    type: 'bar',
-                    barWidth: '60%',
-                    data: [10, 52, 200, 334, 390, 330, 220]
-                  }
-                ]
+              autoResize: 'true'
+            }
+          }
+        })
+        console.log(this.page)
+      }
+      if (this.dragItem === 'chart-line') {
+        components.push({
+          id: id,
+          x: 300,
+          y: 300,
+          width: 350,
+          height: 300,
+          rotate: 0,
+          type: 'chart-line',
+
+          info: {
+            tagName: 'v-echart',
+            style: {
+              position: 'absolute',
+              width: '350px',
+              height: '300px'
+            },
+            options: {
+              tooltip: {
+                trigger: 'axis',
+                axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                  type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                }
               },
+              xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+              },
+              yAxis: {
+                type: 'value'
+              },
+              series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true
+              }]
+            },
+            attrs: {         
               autoResize: 'true'
             }
           }
