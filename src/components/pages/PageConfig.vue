7398634753f9c581b9c846c85811371f0e597eb3
Administrator <skyestzhang@163.com>
2019-10-31 19:26:51 +0800

    rewrite init chart options

--- src/components/pages/PageConfig.vue
+++ src/components/pages/PageConfig.vue
@@ -224,11 +224,11 @@
           <input v-model="currentBox.info.style.width" />
            height:
           <input v-model="currentBox.info.style.height" />
-          <div>
-            <span>图片源:</span>
-            <input v-model="currentBox.info.attrs.src" />
-          </div>
+           <el-checkbox v-model="currentBox.info.props.options.xAxis.axisLabel.show">显示x轴</el-checkbox>
+           <el-checkbox v-model="currentBox.info.props.options.yAxis.axisLabel.show">显示y轴</el-checkbox>
+           <el-checkbox v-model="currentBox.info.props.options.legend.show">显示图例</el-checkbox>
         </div>
+
       </div>
     </div>
 
@@ -364,29 +364,51 @@ export default {
     Vue.component('v-render', {
       render: function (createElement) {
         console.log('componentInfo', this.componentInfo)
-        return createElement(
-          'div',
-          {
-            style: {
-              width: '100%',
-              height: '100%'
-            }
-          },
-          [
-            createElement(
-              this.componentInfo.tagName,
-              {
-                attrs: {
+        if (this.componentInfo.hasOwnProperty('props')) {
+          return createElement(
+            'div',
+            {
+              style: {
+                width: '100%',
+                height: '100%'
+              }
+            },
+            [
+              createElement(
+                this.componentInfo.tagName,
+                {
+                  attrs: this.componentInfo.attrs,
+                  props: {
                   // 为了echart 的属性
-                  options: {...this.componentInfo.options},
-                  ...this.componentInfo.attrs
+                    options: this.componentInfo.props.options
+                  },
+                  style: this.componentInfo.style
                 },
-                style: this.componentInfo.style
-              },
-              this.componentInfo.content
-            )
-          ]
-        )
+                this.componentInfo.content
+              )
+            ]
+          )
+        } else {
+          return createElement(
+            'div',
+            {
+              style: {
+                width: '100%',
+                height: '100%'
+              }
+            },
+            [
+              createElement(
+                this.componentInfo.tagName,
+                {
+                  attrs: this.componentInfo.attrs,
+                  style: this.componentInfo.style
+                },
+                this.componentInfo.content
+              )
+            ]
+          )
+        }
       },
       props: {
         componentInfo: {
@@ -565,45 +587,56 @@ export default {
               width: '350px',
               height: '300px'
             },
-            options: {
-              color: ['#3398DB'],
-              tooltip: {
-                trigger: 'axis',
-                axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                  type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-                }
-              },
-              grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-              },
-              xAxis: [
-                {
+            props: {
+              options: {
+                color: ['#3398DB'],
+                legend: {
+                  show: true
+                },
+                tooltip: {
+                  trigger: 'axis',
+                  axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                  }
+                },
+                grid: {
+                  left: '3%',
+                  right: '4%',
+                  bottom: '3%',
+                  containLabel: true
+                },
+                xAxis: {
                   type: 'category',
                   data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                   axisTick: {
                     alignWithLabel: true
+                  },
+                  axisLabel: {
+                    show: true
                   }
-                }
-              ],
-              yAxis: [
-                {
-                  type: 'value'
-                }
-              ],
-              series: [
-                {
-                  name: '直接访问',
-                  type: 'bar',
-                  barWidth: '60%',
-                  data: [10, 52, 200, 334, 390, 330, 220]
-                }
-              ]
+                },
+
+                yAxis: {
+                  type: 'value',
+                  axisLabel: {
+                    show: true
+                  }
+                },
+
+                series: [
+                  {
+                    name: '直接访问',
+                    type: 'bar',
+                    barWidth: '60%',
+                    data: [10, 52, 200, 334, 390, 330, 220]
+                  }
+                ]
+              },
+              autoResize: 'true'
             },
             attrs: {
-              autoResize: 'true'
+              id: id
+
             }
           }
         })
@@ -626,28 +659,31 @@ export default {
               width: '350px',
               height: '300px'
             },
-            options: {
-              tooltip: {
-                trigger: 'axis',
-                axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                  type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-                }
+            props: {
+              options: {
+                tooltip: {
+                  trigger: 'axis',
+                  axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                  }
+                },
+                xAxis: {
+                  type: 'category',
+                  data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                  type: 'value'
+                },
+                series: [{
+                  data: [820, 932, 901, 934, 1290, 1330, 1320],
+                  type: 'line',
+                  smooth: true
+                }]
               },
-              xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-              },
-              yAxis: {
-                type: 'value'
-              },
-              series: [{
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line',
-                smooth: true
-              }]
-            },
-            attrs: {         
               autoResize: 'true'
+            },
+            attrs: {
+              id: id
             }
           }
         })
