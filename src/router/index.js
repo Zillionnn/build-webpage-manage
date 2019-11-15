ffd37949143e7206c6dd46538cf174592d05d8dc
zillionnn <skyestzhang@163.com>
2019-11-15 16:30:56 +0800

    fix build bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home.vue'
+// import PageConfig from '@/components/pages/PageConfig.vue'
 Vue.use(Router)
 
 export default new Router({
